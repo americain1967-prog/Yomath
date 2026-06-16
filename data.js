defe2fb9bcaa38e5e26fcff3a8acfe343d47eb83
americain1967-prog <americain1967@gmail.com>
2026-06-16 04:57:50 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,33 @@
+const QUESTIONS = [
+{
+q:{
+fr:"Capitale du Burkina Faso ?",
+en:"Capital of Burkina Faso?",
+es:"Capital de Burkina Faso?",
+dioula:"Burkina Faso faso kɛnɛ?",
+moore:"Burkina Faso naam yã?"
+},
+a:["Ouagadougou","Bobo","Paris","Accra"],
+c:0
+},
+{
+q:{
+fr:"2 + 2 = ?",
+en:"2 + 2 = ?",
+es:"2 + 2 = ?",
+dioula:"2 + 2 ?",
+moore:"2 + 2 ?"
+},
+a:["3","4","5","6"],
+c:1
+},
+{
+q:{
+fr:"HTML est un ?",
+en:"HTML is a ?",
+es:"HTML es un ?"
+},
+a:["Langage","Animal","Voiture","Jeu"],
+c:0
+}
+];
