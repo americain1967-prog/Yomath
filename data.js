9f09acf7e62787bbc7ed42a75d767a3473348ae3
americain1967-prog <americain1967@gmail.com>
2026-06-18 23:14:52 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,46 @@
+const DATA = {
+
+math: {
+"6e": [
+{
+name: "Nombres entiers",
+course: "Les entiers servent à compter et comparer... + secrets prof: astuce divisibilité rapide",
+exercises: [
+"1) 12 + 5",
+"2) 345 - 128",
+"3) 456 × 12",
+"4) problème complexe",
+"5) ultra difficile",
+"6) niveau examen"
+],
+homework: [
+"Devoir 1: problème avancé multi-étapes",
+"Devoir 2: raisonnement type concours"
+]
+}
+]
+},
+
+pc: {
+"6e": [
+{
+name: "Matière et état",
+course: "Solide liquide gaz + astuces invisibles profs",
+exercises: ["exo1","exo2","exo3","exo4","exo5","exo6"],
+homework: ["devoir1 ultra","devoir2 examen"]
+}
+]
+},
+
+svt: {
+"6e": [
+{
+name: "Cellule",
+course: "Structure cellule + secrets invisibles prof",
+exercises: ["exo1","exo2","exo3","exo4","exo5","exo6"],
+homework: ["devoir1","devoir2"]
+}
+]
+}
+
+};
